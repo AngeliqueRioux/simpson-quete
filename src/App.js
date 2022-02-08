352ff35c946cd0cd5af31da2ac2483cae38f9f1a
AngéliqueRioux <allange002@gmail.com>
2022-02-08 16:12:51 +0100

    quete Simpson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+const [quote, setQuote] = useState('')
+const [count, setCount] = useState(0)
+
+useEffect(() => {
+
+  axios
+    .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+
+    .then((res) => {
+      setQuote(res.data[0]);
+      console.log(res)
+    });
+}, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={quote.image} alt='simpson' />
+        <p>{quote.quote}</p>
+        <p>{quote.character}</p>
+         <button onClick={() => setCount(prevCount => prevCount + 1)}> Changer la citation</button>
     </div>
   );
 }
